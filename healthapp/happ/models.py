82d643ccc91a44a868173846257f423efcf7d0e1
Edward <edwardgonsalves24@gmail.com>
2018-06-03 00:44:59 +0530

    login

--- healthapp/happ/models.py
+++ healthapp/happ/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class userModel(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	'''
+	alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric')
+	bID = models.CharField(
+		max_length=8, validators=[alphanumeric], primary_key=True
+	)
+	'''
+	age = models.IntegerField()
+
+	def __str__(self):
+		return '%s %s' % (self.user, self.age)
--- healthapp/healthapp/settings.py
+++ healthapp/healthapp/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'happ',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/game/'
+LOGOUT_REDIRECT_URL = '/login/'
